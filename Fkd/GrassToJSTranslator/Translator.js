8dad3582afe6ecc32a59b8e1f40b5b01150cc0b4
FKD <zzzfuku1991@gmail.com>
2012-07-03 03:48:21 +0900

    Translatorの雛形作成

--- Fkd/GrassToJSTranslator/Translator.js
+++ Fkd/GrassToJSTranslator/Translator.js
@@ -2,10 +2,28 @@
 	"use strict";
 	new Function("return this")().Fkd = new Function("return this")().Fkd || {};
 	Fkd.GrassToJSTranslator = Fkd.GrassToJSTranslator || {};
+	Fkd.GrassToJSTranslator.StateMachine = Fkd.GrassToJSTranslator.StateMachine || {};
 	var g = Fkd.GrassToJSTranslator;
+	var s = Fkd.GrassToJSTranslator.StateMachine;
 	var u = Fkd.Utility;
 
-	g.Translator = function() {
-		throw new Error(u.ErrorMessage.notImplemented);
+	g.Translator = function(indentMaker) {
+		if (arguments.length == 1 && indentMaker instanceof g.IndentMaker) {
+			this._indentMaker = indentMaker;
+		} else {
+			throw new Error(u.ErrorMessage.overload);
+		}
+	};
+	g.Translator.prototype.toJS = function(grassCode) {
+		if (arguments.length == 1 && new Object(grassCode) instanceof String) {
+			return this._indentMaker.indent(
+				[
+					"\t\n",
+					new s.Context(new g.IndentMaker(this._indentMaker.indentSize, 2)).parse(grassCode)
+				]
+				.join(""), 0);
+		} else {
+			throw new Error(u.ErrorMessage.overload);
+		}
 	};
 })();
